0786cc105af215bba44fd4a4284254a70dd73191
Nathan <nathancy@hawaii.edu>
2018-06-17 15:38:52 -1000

    Modularized Q2 test driver

--- Q2.py
+++ Q2.py
@@ -33,19 +33,19 @@ def convert(a):
 # ==========================
 # Test Driver 
 # ==========================
+def unitTests():
+    a = "1"
+    b = "1.0"
+    c = "asd"
+    d = "A1B2C3.3;;'"
+    e = "{\{{{PP{O}}"
+    f = "12777777*"
+    g = "+asssssdf"
+    h = "123666.6345"
+    i = "-123"
+    j = "0"
 
-a = "1"
-b = "1.0"
-c = "asd"
-d = "A1B2C3.3;;'"
-e = "{\{{{PP{O}}"
-f = "12777777*"
-g = "+asssssdf"
-h = "123666.6345"
-i = "-123"
-j = "0"
-
-print("""
+    print("""
 Some unit tests are:
 a = "1"
 b = "1.0"
@@ -57,24 +57,27 @@ g = "+asssssdf"
 h = "123666.6345"
 i = "-123"
 j = "0"
-""")
-print("Result from convert function:")
-print(type(convert(a)))
-print(type(convert(b)))
-print(type(convert(c)))
-print(type(convert(d)))
-print(type(convert(e)))
-print(type(convert(f)))
-print(type(convert(g)))
-print(type(convert(h)))
-print(type(convert(i)))
-print(type(convert(j)))
+    """)
+    print("Result from convert function:")
+    print(type(convert(a)))
+    print(type(convert(b)))
+    print(type(convert(c)))
+    print(type(convert(d)))
+    print(type(convert(e)))
+    print(type(convert(f)))
+    print(type(convert(g)))
+    print(type(convert(h)))
+    print(type(convert(i)))
+    print(type(convert(j)))
 
-print(" ")
-print("Custom tester")
-s = str(raw_input("Enter a string to convert ('q' to quit): "))
-while s != 'q':
-    print(s)
-    print(type(convert(s)))
-    s = str(raw_input("Enter string for conversion tester ('q' to quit program): "))
+def customTester():
+    print(" ")
+    print("Custom tester")
+    s = str(raw_input("Enter a string to convert ('q' to quit): "))
+    while s != 'q':
+        print(s)
+        print(type(convert(s)))
+        s = str(raw_input("Enter string for conversion tester ('q' to quit program): "))
 
+unitTests()
+customTester()
