581462a5d480972621bb23136dd76a011533461a
Nathan Lam <nathancy@hawaii.edu>
2018-06-18 04:28:33 -0700

    JSON data and initial implementation

---
+++ Q6.py
@@ -0,0 +1,13 @@
+"""
+File: Q6.py
+Author: Nathan Lam
+Description: Simple GUI implemenation for california cities
+
+Create a simple GUI of city information of California
+- See attached JSON file (ca.json), which contains information of all cities in California.
+- Use the data abstract from the JSON file to create a simple GUI for it
+- The GUI should have a filter function based on the selection of city name (selectable)
+- Display information of county full name, latitude, longitude
+- Sample GUI is shown below
+- Use any python library as needed
+"""
